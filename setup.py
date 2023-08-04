535341f79d2f6496f7421413d266dc2c68b292e2
curamubo <uranuran0713@icloud.com>
2023-08-04 12:19:47 +0900

    little change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name = 's1290018_learn',
-    version = '1.0.0',
+    version = '1.0.1',
     description = 'project is complete.',
     long_description = long_description,
     author = 's1290018',
